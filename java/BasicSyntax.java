e18dbe7538b212603676af5dab9cc86af696f1fe
H-Hp <stepjump3333@yahoo.co.jp>
2016-01-04 09:51:20 +0900

    Javaの基本文法（変数・条件分岐・ループ）を実装

---
+++ java/BasicSyntax.java
@@ -0,0 +1,14 @@
+public class BasicSyntax {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("大人です");
+        } else {
+            System.out.println("未成年です");
+        }
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("ループ回数: " + i);
+        }
+    }
+}
