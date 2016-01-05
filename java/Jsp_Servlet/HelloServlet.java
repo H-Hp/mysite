049df4c7f159575e0091badf569d6d0a0d269f65
H-Hp <stepjump3333@yahoo.co.jp>
2016-01-05 19:27:20 +0900

    JSPの基礎とServlet連携

---
+++ java/Jsp_Servlet/HelloServlet.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello Servlet!</h1>");
+    }
+}
---
+++ java/Jsp_Servlet/index.jsp
@@ -0,0 +1,7 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<html>
+<body>
+  <h2>Hello JSP!</h2>
+  <a href="hello">サーブレットへ</a>
+</body>
+</html>
